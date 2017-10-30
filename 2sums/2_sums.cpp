b7ef0f36b2579777ebc288c3dcb04bc104abb3f6
Sanyam15 <30691340+Sanyam15@users.noreply.github.com>
2017-10-30 15:49:50 +0530

    Update 2_sums.cpp

--- 2sums/2_sums.cpp
+++ 2sums/2_sums.cpp
@@ -5,7 +5,7 @@ public:
         vector <int> res;
         for(int i=0;i<nums.size();i++)
             mp[nums[i]].push_back(i);
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<nums.size();i=i+1)
         {
             int var = target-nums[i];
             if(mp.find(var)==mp.end())
@@ -28,7 +28,7 @@ public:
                 {
                     res.push_back(i);
                     res.push_back(mp[var][1]);
-                    break;
+                     break;
                 }
             }
         }
